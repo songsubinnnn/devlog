177dabb6c3be2ce74523057916c343afa45607e4
songsubinnnn <hui09808@gmail.com>
2025-08-14 17:55:05 +0900

    에러 처리 수정중

---
+++ src/main/java/com/devlog/controller/CustomErrorController.java
@@ -0,0 +1,39 @@
+package com.devlog.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author sbsong
+ * @package com.devlog.controller
+ * @Classname CustomErrorController.java
+ * @Description ""
+ * <PRE>
+ * ---------------------------------
+ * 개정이력
+ * 2025-08-14 sbsong : 최초작성
+ * </PRE>
+ * @since 2025-08-14
+ */
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()){
+                return "error/404";
+            } else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()){
+                return "error/500";
+            }
+        }
+        return "error/default";
+    }
+}
